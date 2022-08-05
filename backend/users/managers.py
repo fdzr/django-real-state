9b12567618700d0cbde22369c604c3241004c1aa
fdzr <fzamora.reina@gmail.com>
2022-08-05 18:05:26 -0400

    adding manager in users app

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -12,14 +12,14 @@ class CustomUserManager(BaseUserManager):
         except Exception as e:
             raise ValidationError(_("You must provide a valid email address"))
 
-    def create_user(self, email, password=None, **kwargs):
-        if not 'username' in kwargs or not kwargs['username']:
+    def create_user(self, username, first_name, last_name, email, password=None, **kwargs):
+        if not 'username':
             raise ValueError(_('Users must submit an username'))
 
-        if not 'first_name' in kwargs or not kwargs['first_name']:
+        if not 'first_name':
             raise ValueError(_('Users must submit a first name'))
 
-        if not 'last_name' in kwargs or not kwargs['last_name']:
+        if not 'last_name':
             raise ValueError(_('Users must submit a last name'))
 
         if email:
@@ -28,16 +28,42 @@ class CustomUserManager(BaseUserManager):
         else:
             raise ValueError(_('Base User Account: An email address is required'))
 
-        user = self.model(email=email, **kwargs)
+        user = self.model(email=email,
+                          first_name=first_name,
+                          last_name=last_name,
+                          username=username,
+                          **kwargs)
         user.set_password(password)
+        kwargs.setdefault("is_staff", False)
+        kwargs.setdefault("is_active", False)
         user.save(using=self._db)
-
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
+    def create_superuser(self, username, first_name, last_name, email, password, **kwargs):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_active", True)
+        kwargs.setdefault("is_superuser", True)
 
+        if kwargs.get("is_staff") is False:
+            raise ValueError(_("Superusers must have is_staff=True"))
+
+        if kwargs.get("is_superuser") is False:
+            raise ValueError(_("Superusers must have is_superuser=True"))
+
+        if not password:
+            raise ValueError(_("Superusers must have a password"))
+
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Admin Account: An email address is required"))
+
+        user = self.create_user(username,
+                                first_name,
+                                last_name,
+                                email,
+                                password,
+                                **kwargs)
+        user.save(using=self._db)
         return user
