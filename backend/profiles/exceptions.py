eeadc8de7210cd9c9240b1538831521f3c9f33ca
fdzr <fzamora.reina@gmail.com>
2022-08-07 16:11:35 -0400

    fixing a bug associated to exceptions file,

--- backend/profiles/exceptions.py
+++ backend/profiles/exceptions.py
@@ -1,11 +1,11 @@
-from rest_framework.exceptions import APIEXception
+from rest_framework.exceptions import APIException
 
 
-class ProfileNotFound(APIEXception):
+class ProfileNotFound(APIException):
     status_code = 404
     default_detail = "The required profile does not exist"
 
 
-class NotYourProfile(APIEXception):
+class NotYourProfile(APIException):
     status_code = 403
     default_detail = "You can't edit a profile that doesn't belong to you"
