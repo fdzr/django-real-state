17d0f641b456ab34b1c0cfee40cf2b730de291af
fdzr <fzamora.reina@gmail.com>
2022-08-12 00:47:32 -0400

    adding entry to property app'sfile endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('supersecret/', admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
-    path("api/v1/profile/", include("profiles.urls"))
+    path("api/v1/profile/", include("profiles.urls")),
+    path("api/v1/properties/", include("properties.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
